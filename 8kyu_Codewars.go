33867ab7df0112e9c4a36e1eac2d4fc4aafe3f09
despx <143245283+despxx@users.noreply.github.com>
2025-06-27 09:25:50 +0300

    Update 8kyu_Codewars.go

--- 8kyu_Codewars.go
+++ 8kyu_Codewars.go
@@ -1,21 +1,10 @@
-// package main
-
-// import "fmt"
-
-// func main() {
-// 	var number int
-// 	fmt.Println("Введите число: ")
-// 	fmt.Scan(&number)
-
-// 	if number%2 == 0 {
-// 		fmt.Println("Even")
-// 	} else {
-// 		fmt.Println("Odd")
-// 	}
-// }
-
-// ---------------------------------------------------
 package kata
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
 
 // MARK: - 8kyu
 
@@ -64,10 +53,115 @@ Sum of positive
 func PositiveSum(numbers []int) (sum int) {
 	for _, num := range numbers {
 		if num > 0 {
-			sum = sum + num
+			sum += num
 		}
 	}
 	return
 }
 
 //---------------------------------------------------
+
+/*
+Reversed Strings
+*/
+
+func Solution(word string) (resultWord string) {
+	for _, w := range word {
+		resultWord = string(w) + resultWord
+	}
+	return
+}
+
+//---------------------------------------------------
+
+/*
+Convert a Number to a String!
+*/
+
+// import "strconv"
+
+func NumberToString(n int) string {
+	var result string = strconv.Itoa(n)
+	return result
+}
+
+//---------------------------------------------------
+
+/*
+Convert boolean values to strings 'Yes' or 'No'.
+*/
+
+func BoolToWord(word bool) string {
+	if word {
+		return "Yes"
+	} else {
+		return "No"
+	}
+}
+
+//---------------------------------------------------
+
+/*
+Square(n) Sum
+*/
+
+func SquareSum(numbers []int) int {
+	var summ int
+	for _, num := range numbers {
+		summ = summ + num*num
+	}
+	return summ
+}
+
+//---------------------------------------------------
+
+/*
+Remove First and Last Character
+*/
+
+func RemoveChar(word string) string {
+	return word[1 : len(word)-1]
+}
+
+//---------------------------------------------------
+
+/*
+Opposite number
+*/
+
+func Opposite(value int) int {
+	return -value
+}
+
+//---------------------------------------------------
+
+/*
+String repeat
+*/
+
+func RepeatStr(repetitions int, value string) string {
+	result := strings.Repeat(value, repetitions)
+	return result
+}
+
+//---------------------------------------------------
+
+/*
+Grasshopper - Summation
+*/
+
+func Summation(n int) int {
+	summ := n * (n + 1) / 2
+	return summ
+}
+
+//---------------------------------------------------
+
+/*
+Find the smallest integer in the array
+*/
+
+func SmallestIntegerFinder(numbers []int) int {
+	sort.Ints(numbers)
+	return numbers[0]
+}
